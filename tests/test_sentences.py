1d96697409b788d220b6d06190819fbb576d7d98
nprezant <34352369+nprezant@users.noreply.github.com>
2019-02-26 17:40:13 -0500

    remove un-used imports

--- tests/test_sentences.py
+++ tests/test_sentences.py
@@ -2,30 +2,25 @@
 import random
 
 from GAlgorithm import (
-    AbstractIndividual, 
-    Population, 
     GeneticAlgorithm,
     initialize_population)
 
-class Sentence(AbstractIndividual):
-    '''List of letters that form a sentence'''
 
-    def compute_fitness(self, target):
-        '''Computes fitness of the string compared to the base string'''
-        fitness = 0
-        for i,g in enumerate(self.genes):
-            if g.value == target[i]:
-                fitness += 1
-        self.fitness = fitness
-        return fitness
+def fitness(sentence, target):
+    '''Computes fitness of the string compared to the base string'''
+    fitness = 0
+    for i,g in enumerate(sentence.genes):
+        if g.value == target[i]:
+            fitness += 1
+    return fitness
 
 
 def run():
     '''runs a new string pattern matching GA'''
     allowed_vals = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !,.'
     target = 'Hello world, I mostly work.'
-    init_pop = initialize_population(500, len(target), allowed_vals, Sentence)
-    ga = GeneticAlgorithm(init_pop, 2, 0.05, 30000)
+    init_pop = initialize_population(500, len(target), allowed_vals)
+    ga = GeneticAlgorithm(init_pop, fitness, 2, 0.05, 30000)
     ga.fitness_params = {'target':target}
     ga.run_without_ml()
 
