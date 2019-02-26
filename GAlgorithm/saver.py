5dde3f9d6b9ce9e51996455d5aea9cd5708a61cf
nprezant <34352369+nprezant@users.noreply.github.com>
2019-02-26 17:36:24 -0500

    clean up un-used code

--- GAlgorithm/saver.py
+++ GAlgorithm/saver.py
@@ -5,8 +5,6 @@ import json
 from pathlib import Path
 from functools import singledispatch
 
-#from tests.tsp_bases import City, Route
-
 @singledispatch
 def to_serializable(o):
     '''used by default. You can register other functions
@@ -18,57 +16,7 @@ def to_serializable(o):
         return o
 
 
-def load(fp):
-    '''reads in a genetic algorithm saved file and plots the data'''
-    with open(fp, 'r') as f:
-        data = f.read()
-
-    dct = json.loads(data)
-    if '__GeneticAlgorithm__' in dct:
-        o = decode_ga(dct)
-    elif '__Route__' in dct:
-        o = decode_route(dct)
-    else:
-        o = dct
-    return o
-
-
 def dump(o, fp):
     '''Saves the object *o* to the *fp* filepath'''
     with open(fp, 'w') as f:
         json.dump(o, f, default=to_serializable, indent=4)
-
-
-def decode_route(dct):
-    '''reads in the starter cities from a saved file'''
-    cities = []
-    for city in dct['_genes']:
-        cities.append(City(city['x'], city['y']))
-    return cities
-
-
-def load_from_files(directory, name_pattern) -> list:
-    '''returns a tuple of the data loaded from
-    all the files found in the directory
-    that matched the name pattern'''
-    paths = []
-    for (dirpath, dirnames, filenames) in os.walk(directory):
-        for name in filenames:
-            if fnmatch.fnmatch(name, name_pattern):
-                paths.append(Path(dirpath, name))
-
-    runs = []
-    for fp in paths:
-        runs.append(load(fp))
-
-    return runs
-
-
-class GASaveObject:
-    def __init__(self):
-        '''An object to hold the save data'''
-        self.best_routes:Route = []
-        self.generations:int = None
-        self.mutation_rate:int = None
-        self.tourny_size:int = None
-        self.classifier:int = None
