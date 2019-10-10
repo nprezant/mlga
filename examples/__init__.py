a2dbdc53febbc64827f0ac17985ad2b50e3d4c28
nprezant <34352369+nprezant@users.noreply.github.com>
2019-10-10 21:36:03 +0200

    re-organized examples folder structure

---
+++ examples/__init__.py
@@ -0,0 +1,3 @@
+from .knapsack import run as run_knapsack
+from .sentences import run as run_sentences
+from .tsp import run as run_tsp
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 
-from tests.tsp import run
+import examples
 
-run()
+examples.run_tsp()
