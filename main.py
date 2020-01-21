cc44be561ccabc2061920db415cc1fafd2db9b79
nprezant <34352369+nprezant@users.noreply.github.com>
2020-01-20 20:04:03 -0500

    Update main.py
    Add Gaussian Naive Bayes ML option

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ from GAlgorithm import (
     Algorithm
 )
 
-n = 3
+n = 100
 folder = Path().cwd() / Path(f'data{n}')
 
 rnd_saves = SaveLocation(folder, 'Random')
 std_saves = SaveLocation(folder, 'Standard')
 ml1_saves = SaveLocation(folder, 'ML_KNN')
 ml2_saves = SaveLocation(folder, 'ML_DT')
+ml3_saves = SaveLocation(folder, 'ML_NB')
 
-run = False
+run = True
 plot = True
 
 # generate GA run data
@@ -32,12 +33,13 @@ if run:
     examples.run_tsp(n, std_saves, algorithm=Algorithm.STANDARD)
     examples.run_tsp(n, ml1_saves, algorithm=Algorithm.ML, classifier_class=KNeighborsClassifier)
     examples.run_tsp(n, ml2_saves, algorithm=Algorithm.ML, classifier_class=DecisionTreeClassifier)
+    examples.run_tsp(n, ml3_saves, algorithm=Algorithm.ML, classifier_class=GaussianNB)
 
 # plot GA data
 if plot:
 
     # set up the save data
-    save_data = SaveData(rnd_saves, std_saves, ml1_saves, ml2_saves)
+    save_data = SaveData(rnd_saves, std_saves, ml1_saves, ml2_saves, ml3_saves)
 
     # fitness data -- different GA types
     _, ax_fitness = plt.subplots()
@@ -49,12 +51,14 @@ if plot:
     _, ax_ml_fitness = plt.subplots()
     save_data.plot_fitness('ML_KNN', ax=ax_ml_fitness)
     save_data.plot_fitness('ML_DT', ax=ax_ml_fitness)
+    save_data.plot_fitness('ML_NB', ax=ax_ml_fitness)
 
     # classifer performance data
     _, ax_good = plt.subplots()
     _, ax_bad = plt.subplots()
     save_data.plot_performance('ML_KNN', ax1=ax_good, ax2=ax_bad, quantiles=10)
     save_data.plot_performance('ML_DT', ax1=ax_good, ax2=ax_bad, quantiles=10)
+    save_data.plot_performance('ML_NB', ax1=ax_good, ax2=ax_bad, quantiles=10)
 
     # best routes
     _, ax_best = plt.subplots()
@@ -62,18 +66,19 @@ if plot:
     save_data.plot_best('Standard', examples.plot_individual_csv, ax_best)
     save_data.plot_best('ML_KNN', examples.plot_individual_csv, ax_best)
     save_data.plot_best('ML_DT', examples.plot_individual_csv, ax_best)
+    save_data.plot_best('ML_NB', examples.plot_individual_csv, ax_best)
 
     # plot stats
     _, ax_stats = plt.subplots()
     save_data.plot_convergence(
-        ['Random', 'Standard', 'ML_KNN', 'ML_DT'],
+        ['Random', 'Standard', 'ML_KNN', 'ML_DT', 'ML_NB'],
         target=630, ax=ax_stats
     )
 
     # plot classifier performance summary
     _, ax_perf = plt.subplots()
     save_data.plot_performance_summary(
-        ['ML_KNN', 'ML_DT'], ax=ax_perf
+        ['ML_KNN', 'ML_DT', 'ML_NB'], ax=ax_perf
     )
 
     # show plots
